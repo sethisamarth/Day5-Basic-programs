1c289fc71c8d51bede6eaaf41c103404e4d223d3
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 22:13:46 +0530

    Sawpping two numbers

---
+++ SwappingNumbers/SwappingNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SwappingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp = 0;
+            Console.WriteLine("Enter the integer values of number1 and number2");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            
+            temp = number1; 
+            number1 = number2;      
+            number2 = temp; 
+            Console.WriteLine("The respective values of number1 is {0},number2 is {1},temp is {2}", number1, number2, temp);
+        }
+    }
+}
