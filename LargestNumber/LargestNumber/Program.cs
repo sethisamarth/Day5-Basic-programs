9f2098f3c78d0eb762044f8d5bcb7b93fc2647c9
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 23:26:25 +0530

    Checking largest no. in triplet

---
+++ LargestNumber/LargestNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter three different numbers ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2 && num1 > num3)
+            {
+                Console.WriteLine("num1 is largest number");
+            } 
+            else if (num2 > num1 && num2 > num3)
+            {
+                Console.WriteLine("num2 is largest number");
+            }
+            else
+            {
+                Console.WriteLine("num3 is largest number");
+
+            }
+        }
+    }
+}
