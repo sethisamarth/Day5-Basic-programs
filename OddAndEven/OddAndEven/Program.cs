be17ded2ce9d8170635f7da574928f0413894960
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 22:51:16 +0530

    Checking odd and even

---
+++ OddAndEven/OddAndEven/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OddAndEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if its even or odd");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("its a even number");
+            }
+            else
+            {
+                Console.WriteLine("its not  even number");
+
+            }
+        }
+    }
+}
