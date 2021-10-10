691f705af2a17997d595a1f1644ed9ff93834869
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 19:18:20 +0530

    sum of harmonic series

---
+++ HarmonicSeries/HarmonicSeries/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HarmonicSeries
+{
+    // sum of 1/1 + 1/2......1/n
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double i = 1; double sum = 0;
+            for (i = 1; i <= n; i++)
+            {
+                 sum += 1 / i; 
+            }
+
+            Console.WriteLine("the sum of the series is :"+sum);
+        }
+    }
+}
