f526e07f47541b69b6f25be6663de86b3013ed66
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 20:00:58 +0530

    prime factorization

---
+++ PrimeFactorization/PrimeFactorization/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PrimeFactorization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the value of n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 2;
+            for (i = 2; i < n * n; i++)
+            {
+                if (n % i == 0)
+                {
+                    while (n % i == 0)
+                    {
+                        Console.WriteLine(i);
+                        n = n / i;
+                    }
+                }
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
