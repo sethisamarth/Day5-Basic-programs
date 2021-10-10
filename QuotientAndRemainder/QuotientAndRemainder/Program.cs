00e3538ce15e66458b5788c2ffa45e547a1476ea
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 21:25:29 +0530

    Checking for Quotient And Remainder

---
+++ QuotientAndRemainder/QuotientAndRemainder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QuotientAndRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Quotient = 0; int Remainder = 0;
+            Console.WriteLine("Enter the value Dividend");
+            int Dividend = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the value Divisor");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
+
+            Quotient = Dividend / Divisor;
+            Remainder= Dividend % Divisor;
+            Console.WriteLine("The value of Quotient and Remainder is {0},{1}", Quotient, Remainder);
+        }
+    }
+}
