283035fd2a9c5b779adfc3c2a0d501a20f21a969
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 23:13:53 +0530

    Check its Vowel or Not

---
+++ VowelOrNot/VowelOrNot/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace VowelOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a alphabet to check its vowel or not");
+            char ch = Convert.ToChar(Console.ReadLine());
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                Console.WriteLine("its a Vowel");
+            }
+            else
+            {
+                Console.WriteLine("its a Consonant");
+            }
+        }
+    }
+}
