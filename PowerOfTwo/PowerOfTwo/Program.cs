8463745f030beb7f8d193adafc58475d6a17b1aa
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-10 18:28:36 +0530

    table of power of two

---
+++ PowerOfTwo/PowerOfTwo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, power = 1;
+            Console.WriteLine("Enter the value of n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (i=1; i<=n; i++)
+            {
+                if (i == 1)
+                {
+                    power = 1;
+                }
+                else
+                {
+                    power = power * 2;
+                }
+                    Console.WriteLine(power);
+            }
+        }
+    }
+}
